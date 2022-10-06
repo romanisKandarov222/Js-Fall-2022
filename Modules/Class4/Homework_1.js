fcde41bedd19b1c4aef1f30e3b8e113705ba8618
romanisKandarov222 <romaiskandarov@gmail.com>
2022-10-06 06:48:50 -0400

    HW_1

--- Modules/Class4/Homework_1.js
+++ Modules/Class4/Homework_1.js
@@ -1,46 +1,68 @@
+/**
+ * Homework - 1
+ * 
+ * Due date : Friday (Oct-7) eod
+ * 
+ * Convert temperature values into different units:
+ * 
+ * F -> C
+ * F -> K
+ * 
+ * C -> F
+ * C -> K
+ * 
+ * K -> F
+ * K -> C
+ * 
+ * Refer link for formula:
+ * https://www.rapidtables.com/convert/temperature/fahrenheit-to-celsius.html
+ * 
+ * 
+ * Extra: Try to find how to get only 2-digits after the decimal point
+ * 
+ */
 
 
 
-
-//Celsius to Kelvin
+//Conversion Celsius to Kelvin
 var celciusTemp = 20;
 var celciusToKelvin = celciusTemp + 273.15;
 var roundedValue = celciusToKelvin.toFixed(2);
-console.log(`\n${celciusTemp} Celsius to Kelvin = ${roundedValue}`);
+console.log(`\n${celciusTemp}°C equals to ${roundedValue}°K`);
 
-//Celsius to Fahrenheit
+//Conversion Celsius to Fahrenheit
 var celciusTemp = 13;
 var celsiusToFahrenheit = celciusTemp * 9/5 + 32;
 var roundedValue = celsiusToFahrenheit.toFixed(2);
-console.log(`\n${celciusTemp} Celsius to Fahrenheit = ${roundedValue}`);
+console.log(`\n${celciusTemp}°C equals to ${roundedValue}°F`);
 
 
 
 
-//Kelvin to Fahrenheit
+//Conversion Kelvin to Fahrenheit
 var kelvinTemp = 310;
 var kelvinToFahrenheit = (kelvinTemp - 273.15) * 9/5 + 32;
 var roundedValue = kelvinToFahrenheit.toFixed(2);
-console.log(`\n${kelvinTemp} Kelvin to Fahrenheit = ${roundedValue}`);
+console.log(`\n${kelvinTemp}°K equals to ${roundedValue}°F`);
 
-//Kelvin to Celsius
+//Conversion Kelvin to Celsius
 var kelvinTemp = 298;
 var kelvinToCelsius = kelvinTemp - 273.15;
 var roundedValue = Math.round(kelvinToCelsius * 100) / 100;
-console.log(`\n${kelvinTemp} Kelvin to Celsius = ${roundedValue}`);
+console.log(`\n${kelvinTemp}°K equals to ${roundedValue}°C`);
 
 
 
 
-//Fahrenheit to Celsius
+//Conversion Fahrenheit to Celsius
 var fahrenheitTemp = 95;
 var fahrenheitToCelsius = (fahrenheitTemp - 32) * 5/9;
 var roundedValue = fahrenheitToCelsius.toFixed(2);
-console.log(`\n${fahrenheitTemp} Fahrenheit to Celsius = ${roundedValue}`);
+console.log(`\n${fahrenheitTemp}°F equals to ${roundedValue}°C`);
 
 
-//Fahrenheit to Kelvin
+//FConversion ahrenheit to Kelvin
 var fahrenheitTemp = 69;
 var fahrenheitToKelvin = (fahrenheitTemp - 32) * 5/9 + 273.15;
 var roundedValue = Math.round(fahrenheitToKelvin * 100) / 100;
-console.log(`\n${fahrenheitTemp} Fahrenheit to Kelvin = ${roundedValue}`);
+console.log(`\n${fahrenheitTemp}°F equals to ${roundedValue}°K`);
