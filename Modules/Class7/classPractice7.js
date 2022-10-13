943a0f886ab1451b4d785258447b93d928c81a9f
romanisKandarov222 <romaiskandarov@gmail.com>
2022-10-13 16:01:37 -0400

    "123"

---
+++ Modules/Class7/classPractice7.js
@@ -0,0 +1,17 @@
+
+var freindsList = ['Tom', 'Steven', 'James', 'Odrey', 'Ann']; // array of String type values
+console.log(`List of my friends -> ${freindsList}\n`);
+var moneyBalance = [123444, 343455, 222, 3544, 5567]; // array of number values
+console.log(`My funds -> ${moneyBalance}\n`);
+
+
+
+
+let studentData = [{name: 'Roman', age: 34}, 
+{name: 'James', age: 23}, 
+{name: 'Odrey', age: 30}, 
+{name: 'Steven', age: 41}];
+
+console.log(`\n${studentData.length}\n`);
+console.log(`\n${studentData[0]}\n`);
+
